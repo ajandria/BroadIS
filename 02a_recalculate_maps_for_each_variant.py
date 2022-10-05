6e6e6fd7fcbd08ccff8fb2495ecf397e05e2ad8d
Adrian Janucik <adrian@MacBook-Pro.local>
2022-10-05 16:10:35 +0200

    add checkpoints

--- 02a_recalculate_maps_for_each_variant.py
+++ 02a_recalculate_maps_for_each_variant.py
@@ -1,5 +1,5 @@
-## 0. Define helper functions
 
+# 0. Define helper functions
 ## Define data wrangling function for synonymous variants
 def train_on_synonymous(ht):
     """
@@ -58,8 +58,30 @@ def regress_per_context(ht, ht_syn_lm):
     # Add MAPS standard error of the mean (sem)
     ht_reg_table_ps_lm_cons_agg_MAPS = ht_reg_table_ps_lm_cons_agg_MAPS.annotate(maps_sem=(ht_reg_table_ps_lm_cons_agg_MAPS.ps_agg * (1 - ht_reg_table_ps_lm_cons_agg_MAPS.ps_agg) / ht_reg_table_ps_lm_cons_agg_MAPS.N_variants) ** 0.5)
     return ht_reg_table_ps_lm_cons_agg_MAPS
+
+def collapse_strand(ht):
+    """
+    Return the deduplicated context by collapsing DNA strands.
     
-## 1. Import packages
+    Function returns the reverse complement if the reference allele is either 'G' or 'T'.
+    
+    The reverse_complement_bases function has been made obsolete and should be replaced by `hl.reverse_complement`.
+    
+    :param ht: Input Table.
+    :return: Table with deduplicated context annotation (ref, alt, context, was_flipped).
+    """
+    collapse_expr = {
+        'ref': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
+                       hl.reverse_complement(ht.ref), ht.ref),
+        'alt': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
+                       hl.reverse_complement(ht.alt), ht.alt),
+        'context': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
+                           hl.reverse_complement(ht.context), ht.context),
+        'was_flipped': (ht.ref == 'G') | (ht.ref == 'T')
+    }
+    return ht.annotate(**collapse_expr) if isinstance(ht, hl.Table) else ht.annotate_rows(**collapse_expr)
+
+# 1. Import packages
 import hail as hl
 from bokeh.io import output_notebook,show
 import gnomad.utils.vep
@@ -68,13 +90,12 @@ import plotly
 import plotly.io as pio
 pio.renderers.default='iframe'
 
-## 2. Import data
+# 2. Import data
 # Import gnomaAD v.3.1.2
 ht = hl.read_table('gs://gcp-public-data--gnomad/release/3.1.2/ht/genomes/gnomad.genomes.v3.1.2.sites.ht')
-ht = ht.head(100000) # Subset the data
 
 # Import mutation rates from gnomAD paper
-ht_mu = hl.import_table('data/supplementary_dataset_10_mutation_rates.tsv.gz',
+ht_mu = hl.import_table('gs://dataproc-staging-us-central1-629456523523-atyywq8w/supplementary_dataset_10_mutation_rates.tsv.gz',
                 delimiter='\t', impute=True, force_bgz=True)
 ht_mu = ht_mu.key_by('context', 'ref', 'alt', 'methylation_level') # has to have a key in order to join using foreign key
 
@@ -84,57 +105,58 @@ context_table = context_table.filter(hl.is_defined(context_table.methyl_mean))
 
 context_table_parsed = context_table.select(context_table.context, context_table.methyl_mean)
 context_table_parsed = context_table_parsed.transmute(context = context_table_parsed.context[2:5])
-
-# Bin methylation mean to methylation levels
 context_table_parsed = context_table_parsed.annotate(methyl_mean = hl.float64(context_table_parsed.methyl_mean))
-context_table_parsed = context_table_parsed.annotate(methylation_level = hl.if_else(context_table_parsed.methyl_mean <= 0.2,0,
-                                                            hl.if_else(context_table_parsed.methyl_mean <= 0.8,1,2)))
+context_table_parsed = context_table_parsed.annotate(methylation_level = hl.case().when(
+            context_table_parsed.methyl_mean > 0.6, 2
+        ).when(
+            context_table_parsed.methyl_mean > 0.2, 1
+        ).default(0))
 
-### Show the data structure
+# Show the data structure
 ht.show(3)
 # Table with methylation level and mutational rate in the trinucleotide context
 ht_mu.show(3)
 # This table contains already precalculated nucleotides -3/+3 from mutation site 
 context_table_parsed.show(3)
 
-## 3. Add context field to main data
+# 3. Add context field to main data
 # Before joining the tri-nucleotide context of mutation
 ht.count()
-# Join only matching rows from context to ht table.
-#ht = ht.key_by('locus', 'alleles').join(context_table_parsed.key_by('locus', 'alleles'), how = 'left') # resorts the data making it slow
-ht = ht.annotate(**context_table_parsed[ht.locus, ht.alleles])
-# After
-ht.count()
-
-# Subset the data for local computation
-ht = ht.filter(hl.is_defined(ht.methylation_level)) # makes sure there is methylation_level variables to be laters used in the grouping
-ht = ht.head(10000) # Subset the data
-ht.show(3)
-
-
-## 4. Add mutation rates for added contexts
-# Split alleles field to ref and alt allele
+# Split alleles field to ref and alt allele and collapse strands
 ht = ht.annotate(ref=ht.alleles[0], alt=ht.alleles[1])
+ht = ht.annotate(**context_table_parsed[ht.locus, ht.alleles])
+ht = collapse_strand(ht) # Collapse strands as some mutation contexts may not be present in mutation table
+# (SNP can be represented as both strands so collapsing makes the schema uniformal) 
+ht.count()
+ht.show(3)
+ht = ht.filter(hl.is_defined(ht.methylation_level)) # Filter to only variants with calculated methylation metric
 
-# Add mutation rates according to the context, but also ref and alt allele for this context
-#ht = ht.key_by("context", "ref", "alt").join(ht_mu.key_by("context", "ref", "alt"), how = 'left') # resorts the data making it slow
+# 4. Add mutation rates for added contexts
+# Add mutation rates according to the context, methylation level but also ref and alt allele for this context
 ht = ht.annotate(**ht_mu[ht.context, ht.ref, ht.alt, ht.methylation_level])
+ht.checkpoint('ht_per_variant.ht')
 
-## 5. Train linear model on synonymous variants for mutational class correction
+# 5. Train linear model on synonymous variants for mutational class correction
 ht_syn_ps = train_on_synonymous(ht)
-ht_syn_ps.show(3)
-
+ht_syn_ps.show()
+ht_syn_ps.count()
 # Perform regression
 ht_syn_lm = ht_syn_ps.aggregate(hl.agg.linreg(ht_syn_ps.ps, [1, ht_syn_ps.mu_snp], weight=ht_syn_ps.N_variants).beta)
 # Show intercept and beta
 ht_syn_lm
 
-## 6. Predict expected number of variants for each context
-### Function for regression eventually will be made starting here and put in `/utils/utils.py` script
+# 6. Predict expected number of variants for each context
 maps_table = regress_per_context(ht, ht_syn_lm)
-maps_table.show(maps_table.count())
+maps_table.checkpoint('maps_table_per_variant.ht')
 
-### Visualise
+maps_table.show(maps_table.count())
 ggplot(maps_table, aes(x=maps_table.consequence, y = maps_table.maps)) + geom_col(aes(fill=maps_table.consequence))
 
+#7. Compute number of variants in each class
+ht_singletons = ht.filter(ht.info.singleton == 1)
+N_singletons = ht_singletons.aggregate(hl.agg.counter(ht_singletons.vep.most_severe_consequence))
 
+N_variants = ht.aggregate(hl.agg.counter(ht.vep.most_severe_consequence))
+
+N_singletons.show(N_singletons.count())
+N_variants.show(N_variants.count())
