a39b72dc166467997d5ef6da77c596a03f60db94
Adrian Janucik <adrian@MacBook-Pro.local>
2022-10-21 21:54:28 +0200

    recalculate maps on dataproc

---
+++ 01_maps/gc_out/data_full_ht/data_full_ht-plots.R
@@ -0,0 +1,30 @@
+
+# Dependencies ------------------------------------------------------------
+library(tidyverse)
+library(ggplot2)
+
+# Setup -------------------------------------------------------------------
+maps_table <- read_csv('01_maps/gc_out/data_full_ht/02a_f_maps_table.csv')
+
+ht_syn_ps <- read_csv('01_maps/gc_out/data_full_ht/ht_syn_ps.csv')
+# Load --------------------------------------------------------------------
+# MAPS bar plot
+maps_full <- ggplot(maps_table,
+       aes(x = consequence, y = maps, fill = consequence)) +
+  geom_bar(stat = 'identity') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ggthemes::theme_hc() +
+  ggtitle('MAPS per variant functional class - full gnomAD.v3')
+
+maps_full %>% 
+  plotly::ggplotly()
+
+# mu_snp vs ps 
+mu_snp_vs_ps <- ht_syn_ps %>%
+  ggplot(aes(x = mu_snp, y = ps, col = context)) +
+  geom_point() +
+  ggthemes::theme_hc() +
+  ggtitle('Singleton proportion vs. mutational rates - Synonymous Variants - full gnomAD.v3')
+
+mu_snp_vs_ps %>% 
+  plotly::ggplotly()
--- 02a_maps_per_variant.py
+++ 02a_maps_per_variant.py
@@ -1,5 +1,13 @@
+# 0. Import packages
+import hail as hl
+from bokeh.io import output_notebook,show
+import gnomad.utils.vep
+from hail.ggplot import *
+import plotly
+import plotly.io as pio
+pio.renderers.default='iframe'
 
-# 0. Define helper functions
+# 1. Define helper functions
 ## Define data wrangling function for synonymous variants
 def train_on_synonymous(ht):
     """
@@ -20,7 +28,7 @@ def train_on_synonymous(ht):
     return ht_syn_ps  
 
 ## Define regression function
-def regress_per_context(ht, ht_syn_lm):
+def regress_per_context(ht, ht_syn_lm, ht_mu):
     """
     Calculates MAPS with Standard Error of the Mean for a given consequence.
     
@@ -42,9 +50,9 @@ def regress_per_context(ht, ht_syn_lm):
 
     # To aggregate just sum for the context
     ht_reg_table_ps_lm_cons_agg = (ht_reg_table_ps_lm_cons.group_by("consequence")
-                  .aggregate(N_singletons=hl.agg.sum(ht_reg_table_ps_lm_cons.N_singletons),
-                             expected_singletons=hl.agg.sum(ht_reg_table_ps_lm_cons.expected_singletons),
-                             N_variants=hl.agg.sum(ht_reg_table_ps_lm_cons.N_variants)))
+                .aggregate(N_singletons=hl.agg.sum(ht_reg_table_ps_lm_cons.N_singletons),
+                            expected_singletons=hl.agg.sum(ht_reg_table_ps_lm_cons.expected_singletons),
+                            N_variants=hl.agg.sum(ht_reg_table_ps_lm_cons.N_variants)))
 
     # Calculate MAPS and aggregated proportions 
     ht_reg_table_ps_lm_cons_agg_MAPS = ht_reg_table_ps_lm_cons_agg.annotate(ps_agg=ht_reg_table_ps_lm_cons_agg.N_singletons / ht_reg_table_ps_lm_cons_agg.N_variants,
@@ -67,106 +75,95 @@ def collapse_strand(ht):
     """
     collapse_expr = {
         'ref': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
-                       hl.reverse_complement(ht.ref), ht.ref),
+                    hl.reverse_complement(ht.ref), ht.ref),
         'alt': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
-                       hl.reverse_complement(ht.alt), ht.alt),
+                    hl.reverse_complement(ht.alt), ht.alt),
         'context': hl.if_else(((ht.ref == 'G') | (ht.ref == 'T')),
-                           hl.reverse_complement(ht.context), ht.context),
+                        hl.reverse_complement(ht.context), ht.context),
         'was_flipped': (ht.ref == 'G') | (ht.ref == 'T')
     }
     return ht.annotate(**collapse_expr) if isinstance(ht, hl.Table) else ht.annotate_rows(**collapse_expr)
 
-# 1. Import packages
-import hail as hl
-from bokeh.io import output_notebook,show
-import gnomad.utils.vep
-from hail.ggplot import *
-import plotly
-import plotly.io as pio
-pio.renderers.default='iframe'
+def main():
 
-# 2. Import data
-# Import gnomaAD v.3.1.2
-ht = hl.read_table('gs://gcp-public-data--gnomad/release/3.1.2/ht/genomes/gnomad.genomes.v3.1.2.sites.ht')
+    # 2. Import data
+    # Import gnomaAD v.3.1.2
+    ht = hl.read_table('gs://gcp-public-data--gnomad/release/3.1.2/ht/genomes/gnomad.genomes.v3.1.2.sites.ht')
 
-#local-run#
-ht = ht.head(1000000)
-ht = ht.checkpoint('data/02a_f_ht_head_100kk.ht')
+    ht = ht.checkpoint('gs://janucik-dataproc-stage/01_maps/data_full_ht/02a_f_ht_head_100kk.ht')
 
-# Import mutation rates from gnomAD paper
-ht_mu = hl.import_table('gs://dataproc-staging-us-central1-629456523523-atyywq8w/supplementary_dataset_10_mutation_rates.tsv.gz',
-                delimiter='\t', impute=True, force_bgz=True)
+    # Import mutation rates from gnomAD paper
+    ht_mu = hl.import_table('gs://janucik-dataproc-stage/01_maps/supplementary_dataset_10_mutation_rates.tsv.gz',
+                    delimiter='\t', impute=True, force_bgz=True)
 
-# Convert mu_snp into string
-ht_mu = ht_mu.key_by('context', 'ref', 'alt', 'methylation_level') # has to have a key in order to join using foreign key
+    # Convert mu_snp into string
+    ht_mu = ht_mu.key_by('context', 'ref', 'alt', 'methylation_level') # has to have a key in order to join using foreign key
 
-# Import context table from gnomad (https://broadinstitute.github.io/gnomad_methods/api_reference/utils/vep.html?highlight=context#gnomad.utils.vep.get_vep_context)
-context_table = gnomad.utils.vep.get_vep_context("GRCh38").ht()
+    # Import context table from gnomad (https://broadinstitute.github.io/gnomad_methods/api_reference/utils/vep.html?highlight=context#gnomad.utils.vep.get_vep_context)
+    context_table = gnomad.utils.vep.get_vep_context("GRCh38").ht()
 
-context_table_parsed = context_table.select(context=context_table.context[2:5], methyl_mean=hl.float64(context_table.methyl_mean))
+    context_table_parsed = context_table.select(context=context_table.context[2:5], methyl_mean=hl.float64(context_table.methyl_mean))
 
-context_table_parsed = context_table_parsed.annotate(methylation_level = hl.case().when(
-            hl.is_missing(context_table_parsed.methyl_mean) == 1, 0
-        ).when(
-            context_table_parsed.methyl_mean > 0.6, 2
-        ).when(
-            context_table_parsed.methyl_mean > 0.2, 1
-        ).default(0))
+    context_table_parsed = context_table_parsed.annotate(methylation_level = hl.case().when(
+                hl.is_missing(context_table_parsed.methyl_mean) == 1, 0
+            ).when(
+                context_table_parsed.methyl_mean > 0.6, 2
+            ).when(
+                context_table_parsed.methyl_mean > 0.2, 1
+            ).default(0))
 
-# Show the data structure
-ht.show(3)
-# Table with methylation level and mutational rate in the trinucleotide context
-ht_mu.show(3)
-print(ht_mu.count())
-# This table contains already precalculated nucleotides -3/+3 from mutation site 
-context_table_parsed.show(3)
+    # Show the data structure
+    ht.show(3)
+    # Table with methylation level and mutational rate in the trinucleotide context
+    ht_mu.show(3)
+    print(ht_mu.count())
+    # This table contains already precalculated nucleotides -3/+3 from mutation site 
+    context_table_parsed.show(3)
 
-# 3. Add context field to main data
-# Before joining the tri-nucleotide context of mutation
-print(ht.count())
-# Split alleles field to ref and alt allele and collapse strands
-ht = ht.annotate(ref=ht.alleles[0], alt=ht.alleles[1], **context_table_parsed[ht.key])
-ht = collapse_strand(ht) # Collapse strands as some mutation contexts may not be present in mutation table
-# (SNP can be represented as both strands so collapsing makes the schema uniformal) 
-print(ht.count())
-ht.show(3)
+    # 3. Add context field to main data
+    # Before joining the tri-nucleotide context of mutation
+    print(ht.count())
+    # Split alleles field to ref and alt allele and collapse strands
+    ht = ht.annotate(ref=ht.alleles[0], alt=ht.alleles[1], **context_table_parsed[ht.key])
+    ht = collapse_strand(ht) # Collapse strands as some mutation contexts may not be present in mutation table
+    # (SNP can be represented as both strands so collapsing makes the schema uniformal) 
+    print(ht.count())
+    ht.show(3)
 
-# 4. Train linear model on synonymous variants for mutational class correction
-ht_synonymous_count = ht.filter(ht.vep.most_severe_consequence == "synonymous_variant")
-ht_synonymous_count.count()
+    # 4. Train linear model on synonymous variants for mutational class correction
+    ht_synonymous_count = ht.filter(ht.vep.most_severe_consequence == "synonymous_variant")
+    ht_synonymous_count.count()
 
-ht_syn_ps = train_on_synonymous(ht)
+    ht_syn_ps = train_on_synonymous(ht)
 
-ht_syn_ps.aggregate(hl.agg.sum(ht_syn_ps.N_variants)) # Check if the numbers are the same for `ht_synonymous_count.count()`
+    ht_syn_ps.aggregate(hl.agg.sum(ht_syn_ps.N_variants)) # Check if the numbers are the same for `ht_synonymous_count.count()`
 
-ht_syn_ps.show()
-print(ht_syn_ps.count())
+    ht_syn_ps.show()
+    print(ht_syn_ps.count())
 
-ht_syn_ps = ht_syn_ps.annotate(**ht_mu[ht_syn_ps.context, ht_syn_ps.ref, ht_syn_ps.alt, ht_syn_ps.methylation_level])
-print(ht_syn_ps.count())
+    ht_syn_ps = ht_syn_ps.annotate(**ht_mu[ht_syn_ps.context, ht_syn_ps.ref, ht_syn_ps.alt, ht_syn_ps.methylation_level])
+    print(ht_syn_ps.count())
 
-# Perform regression
-ht_syn_lm = ht_syn_ps.aggregate(hl.agg.linreg(ht_syn_ps.ps, [1, ht_syn_ps.mu_snp], weight=ht_syn_ps.N_variants).beta)
-# Show intercept and beta
-ht_syn_lm
+    # Perform regression
+    ht_syn_lm = ht_syn_ps.aggregate(hl.agg.linreg(ht_syn_ps.ps, [1, ht_syn_ps.mu_snp], weight=ht_syn_ps.N_variants).beta)
+    # Show intercept and beta
+    ht_syn_lm
 
-# 5. Predict expected number of variants for each context
-maps_table = regress_per_context(ht, ht_syn_lm)
-maps_table = maps_table.checkpoint('maps_table_per_variant.ht')
+    # 5. Predict expected number of variants for each context
+    maps_table = regress_per_context(ht, ht_syn_lm, ht_mu)
+    maps_table = maps_table.checkpoint('gs://janucik-dataproc-stage/01_maps/data_full_ht/maps_table_per_variant.ht')
 
-maps_table_n_rows = maps_table.count()
-maps_table.show(maps_table_n_rows)
+    maps_table_n_rows = maps_table.count()
+    maps_table.show(maps_table_n_rows)
 
-ggplot(maps_table, aes(x = maps_table.consequence, y = maps_table.maps)) + geom_col(aes(fill = maps_table.consequence))
+    # 6. Export tables for plotting and exploring
+    ht.write('gs://janucik-dataproc-stage/01_maps/data_full_ht/02a_f_ht_final.ht')
+    maps_table.export('gs://janucik-dataproc-stage/01_maps/data_full_ht/02a_f_maps_table.csv', delimiter=',')
 
-# 6. Export tables for plotting and exploring
-ht.write('data/02a_f_ht_final.ht')
-maps_table.export('data/02a_f_maps_table.csv', delimiter=',')
+    ht_syn_ps = ht_syn_ps.checkpoint('gs://janucik-dataproc-stage/01_maps/data_full_ht/ht_syn_ps.ht')
+    ht_syn_ps.show(3)
 
-ht_syn_ps = ht_syn_ps.checkpoint('data/ht_syn_ps.ht')
-ht_syn_ps.show(3)
+    ht_syn_ps.export('gs://janucik-dataproc-stage/01_maps/data_full_ht/ht_syn_ps.csv', delimiter=',')
 
-p = hl.plot.scatter(ht_syn_ps.mu_snp, ht_syn_ps.ps, xlabel='Mutation rate', ylabel='Singleton proportion')
-show(p)
-
-ggplot(ht_syn_ps, aes(x = ht_syn_ps.mu_snp, y = ht_syn_ps.ps)) + geom_point(aes(color = ht_syn_ps.context))
+if __name__ == '__main__':
+    main()
