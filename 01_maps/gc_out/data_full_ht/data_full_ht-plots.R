87cade262107cbc7f022a6be42cec9d4349317f8
Adrian Janucik <adrian@MacBook-Pro.local>
2022-10-28 17:48:58 +0200

    try to implement loftee

--- 01_maps/gc_out/data_full_ht/data_full_ht-plots.R
+++ 01_maps/gc_out/data_full_ht/data_full_ht-plots.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(ggplot2)
 
 # Setup -------------------------------------------------------------------
-maps_table <- read_csv('01_maps/gc_out/data_full_ht_26_Oct_22/02a_f_maps_table.csv')
+maps_table <- read_csv('01_maps/gc_out/data_full_ht_26_Oct_22/02a_f_maps_table.csv') %>% 
+  filter(N_variants > 5000)
 
 ht_syn_ps <- read_csv('01_maps/gc_out/data_full_ht_26_Oct_22/ht_syn_ps.csv')
 
@@ -15,25 +16,41 @@ maps_full <- ggplot(maps_table,
   geom_bar(stat = 'identity') +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   ggthemes::theme_hc() +
-  ggtitle('MAPS per variant functional class - full gnomAD.v3')
+  ggtitle('MAPS per variant functional class - based on variants with mu_snp defined and N_variants > 5000 full gnomAD.v3') +
+  coord_flip()
 
 maps_full %>% 
   plotly::ggplotly()
 
 # Recode into variant type
 ht_syn_ps_recoded <- ht_syn_ps %>% 
-  mutate(variant_type = 
-           ifelse((ref == 'G' & alt == 'A' & methylation_level != 0) | (ref == 'C' & alt == 'T' & methylation_level != 0), 'CpG Transition',
-                  ifelse((ref == 'G' & alt == 'A' & methylation_level == 0) | (ref == 'C' & alt == 'T' & methylation_level == 0), 'Non-CpG Transition', 'Transversion'))
-           )
+  mutate(variant_type = case_when(
+    (ref == 'G' & alt == 'A' & methylation_level == 2) | (ref == 'C' & alt == 'T' & methylation_level == 2) ~ 'CpG Transition',
+    (ref == 'G' & alt == 'A' & methylation_level == 0) | (ref == 'C' & alt == 'T' & methylation_level == 0) ~ 'Non-CpG Transition',
+    TRUE ~ 'Transversion'
+  ))
 
 # mu_snp vs ps 
-mu_snp_vs_ps <- ht_syn_ps %>%
-  ggplot(aes(x = mu_snp, y = ps, col = context)) +
+mu_snp_vs_ps <- ht_syn_ps_recoded %>%
+  ggplot(aes(x = mu_snp, y = ps, col = variant_type, fill = context)) +
   geom_point() +
   ggthemes::theme_hc() +
-  ggtitle('Singleton proportion vs. mutational rates - Synonymous Variants - full gnomAD.v3')
+  ggtitle('Singleton proportion vs. mutational rates - Synonymous Variants\nbased on variants with mu_snp defined full gnomAD.v3')
 
 mu_snp_vs_ps %>% 
   plotly::ggplotly()
+
+
+ht_syn_ps_recoded_cpg <- ht_syn_ps_recoded %>% 
+  filter(variant_type == 'Transversion')
+
+ht_syn_ps_recoded_non_cpg <- ht_syn_ps_recoded %>% 
+  filter(variant_type == 'Non-CpG Transition')
+
+
+
+
+
+
+
 
