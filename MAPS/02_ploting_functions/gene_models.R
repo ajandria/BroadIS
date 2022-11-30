1e0159e085928001e9124c3ee116437bed497852
Adrian Janucik <adrian@MacBook-Pro.local>
2022-11-30 18:40:52 +0100

    clean-up

--- MAPS/02_ploting_functions/gene_models.R
+++ MAPS/02_ploting_functions/gene_models.R
@@ -2,10 +2,8 @@
 library(tidyverse)
 library(ggplot2)
 
-maps_global <- read_csv('/Users/adrian/BroadIS/01_maps/data/maps_downsampling_multi_model.csv')
+maps_global <- read_csv('/Users/adrian/BroadIS/MAPS/01_data/maps_clean_global.csv')
 
-maps_global_log_glob <- maps_global %>% 
-  filter(population == 'global')
 
 
 
